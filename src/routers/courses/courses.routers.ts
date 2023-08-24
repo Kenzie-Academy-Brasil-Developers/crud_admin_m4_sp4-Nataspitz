8d07391f40b4348af3f165d8efacb86ae4bd7a14
unknown <nataspitz81@gmail.com>
2023-08-24 17:09:11 -0300

    fix: route get user

--- src/routers/courses/courses.routers.ts
+++ src/routers/courses/courses.routers.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { validateBody, validatePermission, verifyIdCoursForRegister, verifyIdCourse, verifyIdUserForRegister, verifyToken, verifyUserId } from "../../middlewares";
 import { cancelCourseController, createCourseController, listAllCoursesController, listUsersByCourseController, registrationCoursesController } from "../../controllers";
 import { newCourseSchema } from "../../schemas";
+import { verifyAdmin } from "../../middlewares/authorizations/verifyAdmin.moddleware";
 
 export const cousersRoutes: Router = Router() 
 
 cousersRoutes.post("/", verifyToken, validatePermission, validateBody(newCourseSchema), createCourseController)
 cousersRoutes.get("/", listAllCoursesController)
-cousersRoutes.get("/:id/users", verifyToken, validatePermission, verifyIdCourse, listUsersByCourseController)
+cousersRoutes.get("/:id/users", verifyToken, verifyAdmin, verifyIdCourse, listUsersByCourseController)
 cousersRoutes.post("/:courseId/users/:userId", verifyToken, validatePermission , verifyIdCoursForRegister, registrationCoursesController)
 cousersRoutes.delete("/:courseId/users/:userId", verifyToken, validatePermission, verifyIdCoursForRegister, verifyIdUserForRegister, cancelCourseController)
